dd0e3e7ffea9f57701a3a27857a679259296461a
Tony <CodeGuro@gmail.com>
2024-01-08 09:31:27 -0500

    properly name the certificate request with a ".csr" suffix

--- gencrt.sh
+++ gencrt.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # generate the server private key
-KEYNAME=development.key
+PREFIX=development
+KEYNAME=${PREFIX}.key
 openssl genpkey -out "${KEYNAME}" -outform PEM -algorithm EC -pkeyopt ec_paramgen_curve:P-384 -pkeyopt ec_param_enc:named_curve
 if [ $? -ne 0 ]; then
   echo "Failed private key generation"
@@ -10,7 +11,7 @@ fi
 echo "generated server private key: ${KEYNAME}"
 
 # generate the server certificate request
-REQNAME=development.crt
+REQNAME=${PREFIX}.csr
 openssl req -new -keyform PEM -key "${KEYNAME}" -config openssl.cnf -reqexts v3_req -out "${REQNAME}" -subj "/CN=Development/C=US/ST=Area-51/O=Local Development Team"
 if [ $? -ne 0 ]; then
   echo "Failed certificate key generation"
@@ -18,3 +19,6 @@ if [ $? -ne 0 ]; then
 fi
 echo -e "generated server certificate request: ${REQNAME}\n"
 openssl req -noout -text -in "${REQNAME}"
+
+# generate the server certificate from the server certificate request
+CRTNAME=${PREFIX}.crt
