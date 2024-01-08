f06e9b9216e01da407cbdb46b5bbccd253ab9522
Tony <CodeGuro@gmail.com>
2024-01-08 09:42:27 -0500

    clean up

--- old/gencsr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-exec openssl req -new -out development.csr -outform PEM -key development.key -keyform PEM \
-     -subj '/CN=Development/C=US/ST=Area-51/O=Development' \
-     -addext 'subjectAltName=DNS:*attlocal.net,DNS:localhost,IP:127.0.0.1,IP:::1' \
-     -addext 'keyUsage=digitalSignature' \
-     -addext 'basicConstraints=critical,CA:FALSE'
-
--- old/genkey.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-exec openssl genpkey -outform PEM -out development.key \
-  -algorithm EC -pkeyopt ec_paramgen_curve:secp384r1 \
-  -pkeyopt ec_param_enc:named_curve
-
-#openssl genrsa -out development.key 2048
