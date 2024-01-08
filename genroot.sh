0b4c282b8b9608db0294bccc376d6e0d8b8cb706
Tony <CodeGuro@gmail.com>
2024-01-08 09:31:27 -0500

    added code for generation of root key/crt

---
+++ genroot.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# generate the root private key
+KEYNAME=local-root.key
+openssl genpkey -out "${KEYNAME}" -outform PEM -algorithm EC -pkeyopt ec_paramgen_curve:P-384 -pkeyopt ec_param_enc:named_curve
+if [ $? -ne 0 ]; then
+  echo "FAiled private key generation"
+  exit 1
+fi
+echo "generated root private key: ${KEYNAME}"
+
+# generate the root signing certificate
+CRTNAME=local-root.crt
+openssl req -x509 -keyform PEM -key "${KEYNAME}" -days 1000000 -config openssl.cnf -extensions v3_local_ca -out "${CRTNAME}" -subj "/CN=Development"
+if [ $? -ne 0 ]; then
+  echo "Failed public key generation"
+  exit 1
+fi
+echo "generated root certificate: ${CRTNAME}\n\n"
+
+# print out the root certificate details
+openssl x509 -noout -purpose -text -in "${CRTNAME}"
